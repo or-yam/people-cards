650d2f74e8fbf010362825050dcc4240f238af8d
Oryam <48219965+or-yam@users.noreply.github.com>
2020-06-29 21:04:33 +0300

    removed logs added error page

--- main.js
+++ main.js
@@ -4,13 +4,20 @@ const getData = (response) => {
   const source = $('#people-template').html();
   const template = Handlebars.compile(source);
   const newHTML = template(peoples);
-  
-  console.log(peoples);
+
   $('body').append(newHTML);
 };
 
 $.ajax({
+  method: 'GET',
   url: 'https://randomuser.me/api/?results=8',
   dataType: 'json',
   success: getData,
+  error: function (xhr, text, error) {
+    $('#error').append(
+      `<img src="error.jpg" alt="">`
+    );
+    console.log('error');
+  },
 });
+
