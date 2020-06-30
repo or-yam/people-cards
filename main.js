f018ff458c4cb1f351bbbf163d6cc25cb3d1f64e
Oryam <48219965+or-yam@users.noreply.github.com>
2020-06-30 13:36:19 +0300

    handelebars append in seperate function

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-const getData = (response) => {
-  peoples = response.results;
-
+const handleBarsAppend = (data) => {
   const source = $('#people-template').html();
   const template = Handlebars.compile(source);
-  const newHTML = template(peoples);
+  const newHTML = template(data);
 
   $('body').append(newHTML);
+};
+
+const getData = (response) => {
+  const peoples = response.results;
+  handleBarsAppend(peoples);
 };
 
 $.ajax({
@@ -13,11 +16,9 @@ $.ajax({
   url: 'https://randomuser.me/api/?results=8',
   dataType: 'json',
   success: getData,
+
   error: function (xhr, text, error) {
-    $('#error').append(
-      `<img src="error.jpg" alt="">`
-    );
+    $('#error').append(`<img src="error.jpg" alt="">`);
     console.log('error');
   },
 });
-
